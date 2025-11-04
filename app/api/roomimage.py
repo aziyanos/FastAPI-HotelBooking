9b302603d36f6a4edf6cbc5ffc7f8a21c061f189
loren <lorenofq@gmail.com>
2025-11-05 02:52:49 +0600

    add. --> out, create, detail, update

--- app/api/roomimage.py
+++ app/api/roomimage.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from typing import List
 from app.db.models import RoomImage
-from app.db.schemas import RoomImageSchema
+from app.db.schemas import (RoomImageOutSchema, RoomImageCreateSchema,
+                            RoomImageUpdateSchema, RoomImageDetailSchema)
 from app.db.database import SessionLocal
 
 room_image_router = APIRouter(prefix="/room-image", tags=["RoomImage"])
@@ -16,22 +17,23 @@ async def get_db():
         db.close()
 
 
-
-@room_image_router.post("/", response_model=RoomImageSchema)
-async def create_room_image(room_image_data: RoomImageSchema, db: Session = Depends(get_db)):
-    new_room_image = RoomImage(room_image=room_image_data.room_image, room_id=room_image_data.room_id)
+@room_image_router.post("/", response_model=RoomImageOutSchema)
+async def create_room_image(room_image_data: RoomImageCreateSchema,
+                            db: Session = Depends(get_db)):
+    new_room_image = RoomImage(room_image=room_image_data.room_image,
+                               room_id=room_image_data.room_id)
     db.add(new_room_image)
     db.commit()
     db.refresh(new_room_image)
     return new_room_image
 
 
-@room_image_router.get("/", response_model=List[RoomImageSchema])
+@room_image_router.get("/", response_model=List[RoomImageOutSchema])
 async def list_room_images(db: Session = Depends(get_db)):
     return db.query(RoomImage).all()
 
 
-@room_image_router.get("/{room_image_id}/", response_model=RoomImageSchema)
+@room_image_router.get("/{room_image_id}/", response_model=RoomImageDetailSchema)
 async def detail_room_image(room_image_id: int, db: Session = Depends(get_db)):
     room_image_db = db.query(RoomImage).filter(RoomImage.id == room_image_id).first()
     if not room_image_db:
@@ -39,8 +41,9 @@ async def detail_room_image(room_image_id: int, db: Session = Depends(get_db)):
     return room_image_db
 
 
-@room_image_router.put("/{room_image_id}/", response_model=RoomImageSchema)
-async def update_room_image(room_image_data: RoomImageSchema, room_image_id: int, db: Session = Depends(get_db)):
+@room_image_router.put("/{room_image_id}/", response_model=RoomImageOutSchema)
+async def update_room_image(room_image_data: RoomImageUpdateSchema, room_image_id: int,
+                            db: Session = Depends(get_db)):
     room_image_db = db.query(RoomImage).filter(RoomImage.id == room_image_id).first()
     if not room_image_db:
         raise HTTPException(status_code=404, detail="Room Image not found")
