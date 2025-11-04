c328eac3733aa5ebae4dc906a15bf834e7fa960c
loren <lorenofq@gmail.com>
2025-11-05 02:53:19 +0600

    add. --> base(наследование )

--- app/db/schemas.py
+++ app/db/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, EmailStr, validator
+from pydantic import BaseModel, Field, EmailStr, validator, ConfigDict
 from typing import Optional, List
 from app.db.models import RoomStatus, BookingStatus, RoomType, RoleChoices
 from datetime import datetime, date
@@ -16,20 +16,15 @@ class CountryOutSchema(BaseModel):
 
 class CountryCreateSchema(BaseModel):
     country_name: str
-    country_image: str
-
-    class Config:
-        from_attributes = True
+    country_image: str = None
 
 
 class CountryUpdateSchema(BaseModel):
-    country_name: str
-    country_image: str
-
-    class Config:
-        from_attributes = True
+    country_name: Optional[str] = None
+    country_image: Optional[str] = None
 
 
+#/////////////////////////////////////////////////////////
 class UserProfileSchema(BaseModel):
     id: int
     first_name: str
@@ -54,11 +49,10 @@ class UserProfileLoginSchema(BaseModel):
 class UserProfileCreateSchema(BaseModel):
     first_name: str
     last_name: str
-    user_name: str
+    user_name: str = Field(min_length=3, max_length=50)
     age: Optional[int] = Field(None, ge=0, le=150)
     email: EmailStr
-    user_name: str = Field(..., min_length=3, max_length=50)
-    password: str = Field(..., min_length=8)
+    password: str = Field(min_length=8)
 
 
 class UserProfileUpdateSchema(BaseModel):
@@ -71,9 +65,10 @@ class UserProfileUpdateSchema(BaseModel):
 
 class ChangePasswordSchema(BaseModel):
     old_password: str
-    new_password: str = Field(..., min_length=8)
+    new_password: str = Field(min_length=8)
 
 
+#////////////////////////////////////////////////
 class CityBase(BaseModel):
     city_name: str
     city_image: Optional[str] = None
@@ -88,15 +83,28 @@ class CityOut(CityBase):
         from_attributes = True
 
 
-class ServiceSchema(BaseModel):
-    id: int
+#////////////////////////////////////////////////
+
+class ServiceBaseSchema(BaseModel):
     service_name: str
     service_image: str
+
+
+class ServiceSchema(ServiceBaseSchema):
+    id: int
 
     class Config:
         from_attributes = True
 
+class ServiceCreateSchema(ServiceBaseSchema):
+    pass
 
+
+class ServiceUpdateSchema(ServiceBaseSchema):
+    pass
+
+
+#////////////////////////////////////////////////////////////
 class HotelOutSchema(BaseModel):
     id: int
     hotel_name: str
@@ -113,7 +121,6 @@ class HotelOutSchema(BaseModel):
 
 
 class HotelDetailSchema(BaseModel):
-    id: int
     hotel_name: str
     stars: Optional[int] = Field(None, ge=1, le=5)
     postal_index: str
@@ -128,12 +135,12 @@ class HotelDetailSchema(BaseModel):
 
 
 class HotelCreateSchema(BaseModel):
-    hotel_name: str = Field(..., min_length=2, max_length=200)
-    stars: Optional[int] = Field(None, ge=1, le=5)  # от 1 до 5 звёзд
-    postal_index: str = Field(..., min_length=4, max_length=20)
-    description: str = Field(..., min_length=10, max_length=2000)
-    street: str = Field(..., min_length=3, max_length=200)
-    country_id: int = Field(..., gt=0)
+    hotel_name: str = Field(min_length=2, max_length=200)
+    stars: Optional[int] = Field(None, ge=1, le=5)
+    postal_index: str = Field(min_length=4, max_length=20)
+    description: str = Field(min_length=10, max_length=2000)
+    street: str = Field(min_length=3, max_length=200)
+    country_id: int = Field(gt=0) #для положительных чисел gt=0
     city_id: int = Field(..., gt=0)
 
 
@@ -147,36 +154,107 @@ class HotelUpdateSchema(BaseModel):
     city_id: Optional[int] = Field(None, gt=0)
 
 
-class HotelImageSchema(BaseModel):
-    id: int
+#/////////////////////////////////////////////////////////
+class HotelImageBaseSchema(BaseModel):
     hotel_image: str
-
-    class Config:
-        from_attributes = True
+    hotel_id: int
 
 
-class RoomSchema(BaseModel):
+class HotelImageOutSchema(HotelImageBaseSchema):
+    model_config = ConfigDict(from_attributes=True) # удобно для наследование (Pydantic.ver2.0)
+
     id: int
-    room_number: str
+
+
+class HotelImageCreateSchema(BaseModel):
+    hotel_image: str
+    hotel_id: int
+
+
+class HotelImageDetailSchema(HotelImageBaseSchema):
+    pass
+
+
+class HotelImageUpdateSchema(BaseModel):
+    hotel_image: Optional[str] = None
+    hotel_id: Optional[int] = None
+
+
+#class RoomOutSchema(BaseModel):
+#    id: int
+#    room_number: str
+#    room_type: RoomType
+#    room_status: RoomStatus
+#    room_description: str
+#    price: float
+#    max_guests: int
+#
+#    class Config:
+#        from_attributes = True
+
+# важно для наследование
+class RoomBaseSchema(BaseModel):
+    room_number: str = Field(min_length=1, max_length=20)
     room_type: RoomType
     room_status: RoomStatus
-    room_description: str
-    price: float
-    max_guests: int
+    room_description: str = Field(max_length=1000)
+    price: float = Field(gt=0)
+    max_guests: int = Field(gt=0, le=100)
+
+
+class RoomCreateSchema(RoomBaseSchema):
+    pass
+
+
+class RoomDetailSchema(RoomBaseSchema):
+    pass
+
+
+class RoomUpdateSchema(BaseModel):
+    room_number: Optional[str] = Field(None, min_length=1, max_length=20)
+    room_type: Optional[RoomType] = None
+    room_status: Optional[RoomStatus] = None
+    room_description: Optional[str] = Field(None, max_length=1000)
+    price: Optional[float] = Field(None, gt=0)
+    max_guests:  Optional[int] = Field(None, gt=0, le=10)
+
+
+class RoomOutSchema(RoomBaseSchema):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+
+
+#/////////////////////////////
+class RoomImageOutSchema(BaseModel):
+    id: int
+    room_image : str
+    room_id: int
 
     class Config:
         from_attributes = True
 
 
-class RoomImageSchema(BaseModel):
-    id:int
+class RoomImageBaseShema(BaseModel):
     room_image: str
-
-    class Config:
-        from_attributes = True
+    room_id: int
 
 
-class BookingSchema(BaseModel):
+class RoomImageCreateSchema(RoomImageBaseShema):
+    pass
+
+
+class RoomImageDetailSchema(RoomImageBaseShema):
+    pass
+
+
+class RoomImageUpdateSchema(BaseModel):
+    room_image: Optional[str] = None
+    room_id: Optional[int] = None
+
+
+#///////////////////////////////////////////////////
+class BookingOutSchema(BaseModel):
     id:int
     check_in: date
     check_out: date
@@ -189,28 +267,95 @@ class BookingSchema(BaseModel):
         from_attributes = True
 
 
-class ReviewSchema(BaseModel):
-    id: int
-    stars: Optional[int] = Field(None, ge=1, le=5)
-    comment: Optional[str] = None
-
-    class Config:
-        from_attributes = True
-
-
-class FavouriteSchema(BaseModel):
-    id: int
+class BookingCreateSchema(BaseModel):
+    check_in: date
+    check_out: date
+    booking_status: BookingStatus
+    hotel_id: int
+    room_id: int
     user_id: int
 
 
+class BookingDetailSchema(BookingOutSchema):
+    pass
+
+
+class BookingUpdateSchema(BaseModel):
+    check_in: Optional[date] = None
+    check_out: Optional[date] = None
+    booking_status: Optional[BookingStatus] = None
+    hotel_id: Optional[int] = None
+    room_id: Optional[int] = None
+
+
+#///////////////////////////////////////////////////
+class ReviewOutSchema(BaseModel):
+    id: int
+    stars: Optional[int] = Field(None, ge=1, le=5)
+    comment: Optional[str] = Field(None, max_length=1000)
+    hotel_id : int
+    user_id: int
+
     class Config:
         from_attributes = True
 
 
-class FavouriteItemSchema(BaseModel):
+class ReviewCreateSchema(BaseModel):
+    stars: Optional[int] = Field(None, ge=1, le=5)
+    comment: Optional[str] = Field(None, max_length=1000)
+    hotel_id : int
+    user_id: int
+
+
+class ReviewUpdateSchema(BaseModel):
+    stars: Optional[int] = Field(None, ge=1, le=5)
+    comment: Optional[str] = Field(None, max_length=1000)
+
+
+#////////////////////////////////////////////////////
+class FavouriteBaseSchema(BaseModel):
+    user_id: int
+
+
+class FavouriteOutSchema(FavouriteBaseSchema):
     id: int
+
+    class Config:
+        from_attributes = True
+
+
+class FavouriteCreateSchema(FavouriteBaseSchema):
+    pass
+
+
+class FavouriteDetailSchema(FavouriteBaseSchema):
+    pass
+
+
+class FavouriteUpdateSchema(FavouriteBaseSchema):
+    pass
+
+
+#////////////////////////////////////////////////////////
+class FavouriteItemBaseSchema(BaseModel):
     favourite_id: int
     hotel_id: int
 
+
+class FavouriteItemOutSchema(FavouriteItemBaseSchema):
+    id: int
+
     class Config:
         from_attributes = True
+
+
+class FavouriteItemCreateSchema(FavouriteItemBaseSchema):
+   pass
+
+
+class FavouriteItemDetailSchema(FavouriteItemBaseSchema):
+    pass
+
+
+class FavouriteItemUpdateSchema(FavouriteItemBaseSchema):
+    pass
